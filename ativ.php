4a918017fea6c070a547413c52a1da7cf6842ccc
bernardinhookkj <142499089+BernardoBWeiss@users.noreply.github.com>
2025-03-17 17:34:31 +0000

    a

---
+++ ativ.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Escolha uma Opção:";
+echo "1 - Dar Boas Vindas";
+echo "2 - Dar Mensagem";
+echo "3 - Seu Gosto";
+echo "4 - Dar Tchau";
+
+$opcao = $_POST["Digite o Número da sua Opção:"];
+
+?>
